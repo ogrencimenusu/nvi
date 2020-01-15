d5816960d39200b10fb4ae21b4b70464186b505f
ogrencimenusu <srht.akyol.1649@gmail.com>
2020-01-15 16:35:39 +0300

    login sayfası

---
+++ assets/js/theme.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        draggable: false,
+        arrows: false,
+        dots: false,
+        fade: true,
+        speed: 3000,
+        infinite: true,
+        cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+        touchMove:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+});
